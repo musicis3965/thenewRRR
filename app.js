7501e12e1ed140764bfa014f7c13a3bc5f729bfa
musicis3965 <bobby.timmons@rocketmail.com>
2019-02-14 01:27:19 -0500

    push

--- app.js
+++ app.js
@@ -326,6 +326,6 @@ req.login(user, function(err){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
